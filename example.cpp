3a3ff9d51faf1429f441cdaacfc27270fa0b72a1
Mehdi Hashemian <mehdi@forallsecure.com>
2022-07-13 16:53:40 -0700

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -2,8 +2,9 @@
 #include <cpr/cpr.h>
 
 int main(int argc, char** argv) {
-#    cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},\
-     std::cout << argv[1] << std::endl;
+    std::cout << argv[1] << std::endl;
+     //    cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
+     
      cpr::Response r = cpr::Get(cpr::Url{argv[1]},
                       cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
                       cpr::Parameters{{"anon", "true"}, {"key", "value"}});
