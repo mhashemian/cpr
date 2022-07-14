3a6566b2e3c4189d3a64c5edcd1fce89f867c72e
Mehdi Hashemian <mehdi@forallsecure.com>
2022-07-14 14:30:03 -0700

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -4,7 +4,7 @@
 #include <cpr/cpr.h>
 
 int main(int argc, char** argv) {
-    std::cout << argv[1] << std::endl;
+    //std::cout << argv[1] << std::endl;
     // cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
     
     std::ifstream inFile;
@@ -14,7 +14,7 @@ int main(int argc, char** argv) {
     strStream << inFile.rdbuf();
     std::string str = strStream.str(); //str holds the content of the file
 
-    std::cout << str << "\n";
+    //std::cout << str << "\n";
     
     cpr::Response r = cpr::Get(cpr::Url{str},
                       cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
