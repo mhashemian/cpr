dc130e9dcbdbb519a4f0f8d316a0b12f4e57ca43
Mehdi Hashemian <mehdi@forallsecure.com>
2022-07-14 15:09:34 -0700

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -5,24 +5,24 @@
 
 int main(int argc, char** argv) {
     //std::cout << argv[1] << std::endl;
-    // cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
     
-    std::ifstream inFile;
-    inFile.open(argv[1]);
+    
+    //std::ifstream inFile;
+    //inFile.open(argv[1]);
 
-    std::stringstream strStream;
-    strStream << inFile.rdbuf();
-    std::string str = strStream.str(); //str holds the content of the file
+    //std::stringstream strStream;
+    //strStream << inFile.rdbuf();
+    //std::string str = strStream.str(); //str holds the content of the file
 
     //std::cout << str << "\n";
-    
-    cpr::Response r = cpr::Get(cpr::Url{str},
+    cpr::Response r = cpr::Get(cpr::Url{"tcp://localhost:8080"},
+    //cpr::Response r = cpr::Get(cpr::Url{str},
                       cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
                       cpr::Parameters{{"anon", "true"}, {"key", "value"}});
     r.status_code;                  
     r.header["content-type"];       
     r.text;
     
-    inFile.close();
+    //inFile.close();
     return 0;
 }
